16188cfcfa320f97c4b617204250966cc09f56c3
redsolofox <RedSoloFox@gmail.com>
2017-05-08 02:06:02 -0400

    Woops forgot to fix the ver num

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyGeckoWiiU',
-    version='1.0.1',
+    version='0.0.1',
     packages=find_packages(),
     url='https://github.com/RedSoloFox/pyGecko',
     license='MIT',
